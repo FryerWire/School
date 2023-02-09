c44165fad7b76764e6b6e389d2814dc5bbac1368
FryerWire <69731415+FryerWire@users.noreply.github.com>
2023-02-09 15:21:54 -0600

    added more data to the print

--- AERO-161/Projects/Project 1/question_2.py
+++ AERO-161/Projects/Project 1/question_2.py
@@ -28,12 +28,16 @@ density_alititude = field_elevation + (145366 * (1 - (((17.326 * pressure_Hg) / 
 
 print(f"\nMeasurements data gathered from current weather data")
 print(52 * '-')
-print(f"Dew Point           : {round(dew_point, 5)} [deg C]")
-print(f"Vapor Pressure      : {round(vapor_pressure, 5)} [mbar]")
-print(f"Virtual Temperature : {round(virtual_temp_K, 5)} [K]")
-print(f"Virtual Temperature : {round(virtual_temp_R)} [R]")
-print(f"Pressure            : {round(pressure_Hg)} [in-Hg]")
-print(f"Density Altitude    : {round(density_alititude)} [ft]")
+print(f"Current Temperature : {round(current_temperature, 2)} [K]")
+print(f"Current Temperature : {round(temperature_C, 2)} [deg C]")
+print(f"Current Pressure    : {round(current_pressure, 2)} [mbar]")
+print(f"Current Humidity    : {round(current_humidity, 2)} [%]")
+print(f"Dew Point           : {round(dew_point, 2)} [deg C]")
+print(f"Vapor Pressure      : {round(vapor_pressure, 2)} [mbar]")
+print(f"Virtual Temperature : {round(virtual_temp_K, 2)} [K]")
+print(f"Virtual Temperature : {round(virtual_temp_R, 2)} [deg R]")
+print(f"Pressure            : {round(pressure_Hg, 2)} [in-Hg]")
+print(f"Density Altitude    : {round(density_alititude, 2)} [ft]")
 print(52 * '-')
 
 
